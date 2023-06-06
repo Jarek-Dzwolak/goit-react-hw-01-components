8313935493a645159ab92519c7108c53849ad8d2
Jarek Dzwolak <jaroslawdzwolak@gmail.com>
2023-06-06 22:48:57 +0200

    FristSTepInReact

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import FriendListItem from './FriendListItem'; // Import komponentu FriendListItem
+
+const FriendList = ({ friends }) => (
+  <ul className="friend-list">
+    {friends.map(({ avatar, name, isOnline, id }) => (
+      <FriendListItem
+        key={id}
+        avatar={avatar}
+        name={name}
+        isOnline={isOnline}
+      />
+    ))}
+  </ul>
+);
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ).isRequired,
+};
+
+export default FriendList;
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+
+const Statistics = ({ stats }) => {
+  return (
+    <section className="statistics">
+      <h2 className="title">Upload stats</h2>
+
+      <ul className="stat-list">
+        {stats.map(item => (
+          <li key={item.id} className="item">
+            <span className="label">{item.label}</span>
+            <span className="percentage">{item.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+export default Statistics;
